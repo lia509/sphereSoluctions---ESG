7e32e9adf3fb82f7e051f6541fdb98cf3cb094f5
Samuel Pinheiro <125027456+samuelpinheiro564@users.noreply.github.com>
2024-04-29 14:59:49 -0300

    Atualizar estilos e adicionar tela de login

---
+++ src/screens/Login/index.jsx
@@ -0,0 +1,49 @@
+export default function Login() {
+
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+
+  const Entrar = () =>{
+  console.log("passou aqui");
+    const login = ListaEmps.getLogin(email,senha);
+    console.log(login);
+    if(nome == '' || email == '' || senha== ''){
+   alert("Preencha todos os campos");
+  }else{
+  if(!login){
+    navigation.navigate('Home');
+  }else{
+    alert("Email ou senha incorretos");
+  }
+}  
+}
+  return (
+    <View style={styles.container}>
+      <Title title="Login"/>
+
+<TextInput 
+style={styles.input} 
+placeholder="Nome"
+onChangeText={setNome}
+value={nome}/>
+<View style={styles.linha} ></View>
+<TextInput 
+style={styles.input} 
+placeholder="email"
+onChangeText={setEmail}
+value={email}/>
+<View style={styles.linha} ></View>
+<TextInput 
+style={styles.input}
+ placeholder="senha"
+onChangeText={setSenha}
+value={senha}/>
+<View style={styles.linha} ></View>
+<TouchableOpacity  onPress={Entrar} >
+  <Text>Entrar</Text></TouchableOpacity>
+<TouchButton route="Cadastro" title="Cadastrar" />
+    </View>
+  );
+}
---
+++ src/screens/Login/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#C9D4FF",
+    borderRadius: 5,
+  },
+  input: {
+    width: "70%",
+    margin: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  linha: {
+    height: 3, 
+    width: '70%',  
+    marginTop: 5,
+    marginBottom: 5,
+    backgroundColor: 'Black',
+  },
+});
+
+export default styles;
